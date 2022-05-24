beb86aacc35a8d65cfb67ed45d76257d85585ebd
rifatjahan823 <rifatjahan823@gmail.com>
2022-05-24 18:02:15 +0600

    add sweet alert

--- src/Components/Pages/Dashboard/ProductsRow.js
+++ src/Components/Pages/Dashboard/ProductsRow.js
@@ -1,33 +1,51 @@
 import React from 'react';
 import { toast } from 'react-toastify';
-import swal from 'sweetalert';
+import Swal from 'sweetalert2';
 
 const ProductsRow = ({product,refetch}) => {
     const {picture,name ,price,_id}=product;
     const handleDelete = (Id)=>{
-        fetch(`http://localhost:5000/addProduct/${Id}`,{
-            method:"Delete",
-            headers:{
-                authorization:`Bearer ${localStorage.getItem('accessToken')}`
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+          if (result.isConfirmed) {           
+    fetch(`http://localhost:5000/addProduct/${Id}`,{
+        method:"Delete",
+        headers:{
+            authorization:`Bearer ${localStorage.getItem('accessToken')}`
+        }
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        if(data.deletedCount){
+             refetch()
+        }
+        else{
+            toast.error(`Product ${name} Not Deleteed` )     
+        }
+    })
+      Swal.fire(
+             'Deleted!',
+            'Your file has been deleted.',
+             'success'
+              )
             }
-        })
-        .then(res=>res.json())
-        .then(data=>{
-            if(data.deletedCount){
-                swal("Hello world!");
-                 refetch()
-            }
-            else{
-                toast.error(`Product ${name} Not Deleteed` )     
-            }
-        })
+          });
+      
+
     }
     return (
         <tr>
         <td><img style={{width:"90px",height:"90px",borderRadius:"50px",border:"1px solid #CDCCCC"}} className='img-fluid' src={picture} alt="" /></td>
         <td>{name}</td>
         <td>{price}</td>
-        <td><button  onClick={()=> handleDelete(product._id)} className="btn btn-danger">Remove User</button></td> 
+        <td><button  onClick={()=> handleDelete(_id)} className="btn btn-danger">Remove User</button></td> 
     </tr>
     );;
 };
